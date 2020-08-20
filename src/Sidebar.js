b6d87d19690bc8eeaeb00bda9977811f1feb3067
Semir Cohovic <semircohovic@gmail.com>
2020-08-20 20:52:43 +0200

    material ui and icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Avatar } from "@material-ui/core";
+
 import './Sidebar.css'
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-
+                <Avatar />
             </div>
             <div className="sidebar__search">
 
             </div>
+            <div className="sidebar__chats"></div>
         </div>
     )
 }
