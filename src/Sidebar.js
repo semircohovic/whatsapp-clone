09c2634c54bc04acc7a842308ca260ff38e90434
Semir Cohovic <semircohovic@gmail.com>
2020-08-20 21:22:22 +0200

    sidebar icons avatar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { Avatar } from "@material-ui/core";
-
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcom from "@material-ui/icons/MoreVert";
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+import SidebarChat from './SidebarChat';
 import './Sidebar.css'
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcom />
+                    </IconButton>
+
+                </div>
             </div>
             <div className="sidebar__search">
-
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
             </div>
-            <div className="sidebar__chats"></div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+            </div>
         </div>
     )
 }
---
+++ src/SidebarChat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SidebarChat.css';
+import { Avatar } from '@material-ui/core';
+function SidebarChat() {
+    return (
+        <div className="sidebarChat">
+            <Avatar src="https://avatars.dicebear.com/api/male/.svg" />
+            <div className="sidebarChat__info">
+                <h2>Room name</h2>
+                <p>Last message ...</p>
+            </div>
+        </div>
+    )
+}
+
+export default SidebarChat
