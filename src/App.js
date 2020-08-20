0a6c58cec623c29a6ff9fd855d29e870809600b0
Semir Cohovic <semircohovic@gmail.com>
2020-08-20 20:48:48 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
 
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyByBBOj1iTDTVZ1qh9_g6G03BXS9uhf0mk",
+    authDomain: "whatsapp-clone-d8cfa.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-d8cfa.firebaseio.com",
+    projectId: "whatsapp-clone-d8cfa",
+    storageBucket: "whatsapp-clone-d8cfa.appspot.com",
+    messagingSenderId: "263192423055",
+    appId: "1:263192423055:web:8a75610d7296d8b68b9efa",
+    measurementId: "G-HMHW3RYJMC"
+};
